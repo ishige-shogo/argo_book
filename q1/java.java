c09278f8fe70a89ddc14df6592c4e43f385141b2
ishige-shogo <shigeshigeishige@gmail.com>
2021-02-09 08:43:45 +0900

    pra

---
+++ q1/java.java
@@ -0,0 +1,15 @@
+public class java {
+    public static void main(String[] args) {
+        int val = 2;
+        switch (val) {
+        case 3:
+            System.out.println("OK");
+            break;
+        case 2:
+            System.out.println("SOso");
+            break;
+        default:
+            System.out.println("NG");
+        }
+    }
+}
