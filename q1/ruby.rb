ff3e00b063155f43348a83271149323917876a74
ishige-shogo <shigeshigeishige@gmail.com>
2021-02-09 09:35:49 +0900

    pra

--- q1/ruby.rb
+++ q1/ruby.rb
@@ -1,1 +1,15 @@
-a
+
+people = 100
+
+way = 0
+
+0.upto(people) do |rock|
+    0.upto(people-rock) do |paper|
+        scissor = people - rock - paper
+        all = [rock, paper, scissor]
+        way += 1 if all.count(all.max) == 1
+    end
+end
+
+puts way
+git a
